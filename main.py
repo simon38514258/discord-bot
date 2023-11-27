bc758d500c6ea115efc18c0562c81c695da4842b
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2023-11-28 01:50:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-#導入 Discord.py
+#導入 Discord.py 及 from discord.ext import commands
 import discord
-#client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
+from discord.ext import commands
+
+#bot 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="@mei",intents=intents)
+@bot.event
 
-#調用 event 函式庫
-@client.event
 #當機器人完成啟動時
 async def on_ready():
-    print('目前登入身份：咩0.0', client.user)
+    print('目前登入身份：咩0.0', bot.user)
     game = discord.Game('窩ㄞ咩咩')#暱稱欄位文字
     #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
-    await client.change_presence(status=discord.Status.online, activity=game)
+    await bot.change_presence(status=discord.Status.online, activity=game)
 
-@client.event
-#當有訊息時
-async def on_message(message):
-    #排除自己的訊息，避免陷入無限循環
-    if message.author == client.user:
-        return
-    #如果包含 ping，機器人回傳 pong
-    if message.content == 'ping':
-        await message.channel.send('pong')
 
-client.run('MTE3ODY4NjI0NDQxOTU1MTI2Mw.GLjhH7.B2Kmi-7ERkzbiDOg7jBvdWOi_a582bhPzt4Wyo') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+#TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
+bot.run('MTE3ODY4NjI0NDQxOTU1MTI2Mw.GLjhH7.B2Kmi-7ERkzbiDOg7jBvdWOi_a582bhPzt4Wyo') 
